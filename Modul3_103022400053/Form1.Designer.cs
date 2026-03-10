99717cf9d36140e3290c6de0a8ca12d30c4cc71f
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-03-11 06:57:00 +0700

    Menambahkan tampilan GUI

--- Modul3_103022400053/Form1.Designer.cs
+++ Modul3_103022400053/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(69, 117);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(109, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(69, 203);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(109, 33);
+            comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Satuan awal";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(69, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Satuan akhir";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(205, 119);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(321, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.InactiveCaption;
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(205, 205);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(321, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(205, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 25);
+            label3.TabIndex = 6;
+            label3.Text = "Nilai awal";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(205, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 25);
+            label4.TabIndex = 7;
+            label4.Text = "Nilai akhir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
     }
 }
